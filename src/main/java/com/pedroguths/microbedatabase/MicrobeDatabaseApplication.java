977f4308e9c0182380e731efe48f4f3803b497a3
Pedro Henrique Valente Guths <guthspedro@gmail.com>
2026-07-20 19:41:36 -0300

    feat: initial structure for spring boot

---
+++ src/main/java/com/pedroguths/microbedatabase/MicrobeDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.pedroguths.microbedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrobeDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrobeDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pedroguths/microbedatabase/MicrobeDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroguths.microbedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrobeDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
